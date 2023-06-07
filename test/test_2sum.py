ecded3246e2695777c7e36cade473fec2c915848
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-07 12:01:58 +0100

    code refactoring🎌

--- test/test_2sum.py
+++ test/test_2sum.py
@@ -4,7 +4,8 @@
 # @Author : AllisonOge
 
 import unittest
-from ..two_sum import two_sum
+from python.two_sum import two_sum
+
 
 class Test2Sum(unittest.TestCase):
     def test_empty_list(self):
@@ -12,12 +13,13 @@ class Test2Sum(unittest.TestCase):
 
     def test_no_pair(self):
         self.assertEqual(two_sum([2, 7, 11, 15], 3), [])
-    
+
     def test_one_pair(self):
         self.assertEqual(two_sum([2, 7, 11, 15], 9), [0, 1])
 
     def test_multiple_pairs(self):
         self.assertEqual(two_sum([2, 7, 11, 15], 17), [0, 3])
+
 
 if __name__ == '__main__':
     unittest.main()
--- test/test_magic_square.py
+++ test/test_magic_square.py
@@ -4,17 +4,21 @@
 # @Author : AllisonOge
 
 import unittest
-from ..magic_square import forming_magic_square
+from python.magic_square import forming_magic_square
+
 
 class TestMagicSquare(unittest.TestCase):
     def test_empty_matrix(self):
         self.assertEqual(forming_magic_square([]), float("inf"))
 
     def test_costly_matrix(self):
-        self.assertEqual(forming_magic_square([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), 20)
-    
+        self.assertEqual(forming_magic_square(
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]]), 20)
+
     def test_cheap_matrix(self):
-        self.assertEqual(forming_magic_square([[4, 9, 2], [3, 5, 7], [8, 1, 5]]), 1)
+        self.assertEqual(forming_magic_square(
+            [[4, 9, 2], [3, 5, 7], [8, 1, 5]]), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
